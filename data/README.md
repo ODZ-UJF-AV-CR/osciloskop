cacabf7d7728c952d1f41e3afd207451cae60e8c
kakl <martin.kakona@gmail.com>
2017-08-04 11:52:44 +0200

    last shift version before SPS stoped working

--- data/README.md
+++
@@ -1,4 +0,0 @@
-Directory for captured data.
-energy in dir name is measured on beamline
-
-Note that data files are removed on each run.
--- data/newrun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TD=$(date +%y%m%d-%H%M%S-$1)
-echo Moving current data to $TD
-mkdir $TD || exit 1
-mv -v data* $TD
-cp README $TD
-scp -r $TD hroch.ujf.cas.cz:/volume1/public/cosmic/Experiments/2017/06_himac/
