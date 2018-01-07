608ea9ee9c3455679c2fc5ca949db9f146d590dc
Vaclav Stepan <vaclav.stepan@gmail.com>
2018-01-07 15:30:13 +0900

    Monitor events in the GPS logfile

--- gpsmon.sh
+++ gpsmon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while (sleep 2 ) ; do N=$( ls -1rt *.gps | tail -1 ) ; echo -n $N " " ; ../../pyUblox/ublox_show.py $N | grep TM2 | wc -l ; done
+while (sleep 2 ) ; do clear; N=$( ls -1rt *.gps | tail -1 ) ; echo -n $N " " ; ../../pyUblox/ublox_show.py $N | grep TM2 | wc -l  ; done
